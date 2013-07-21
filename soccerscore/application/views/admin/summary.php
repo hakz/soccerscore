a11d30d42a0912311c78b010d92d4315443409f8
ikhsan@students.amikom.ac.id <ikhsan@students.amikom.ac.id@ee350e63-9e96-02d9-0549-f21851359956>
2013-07-21 01:57:39 +0000

    asd

--- soccerscore/application/views/admin/summary.php
+++ soccerscore/application/views/admin/summary.php
@@ -2,92 +2,61 @@
 echo '<pre>';
 print_r($summary);
 echo '</pre>';
+
 ?>
 
 <div class="row-fluid">
-    <div class="accordion" id="accordion2">
+	<?php foreach ($summary as $key => $s) { ?>
+		 <div class="accordion" id="accordion<?php echo $key+1?>">
         <div class="accordion-group">
-            <div class="accordion-heading"><a class="accordion-toggle" href="#collapse1" data-toggle="collapse" data-parent="#accordion2">England</a></div>
-            <div class="accordion-body collapse in" id="collapse1">
+            <div class="accordion-heading"><a class="accordion-toggle" href="#collapse<?php echo $key+1?>" data-toggle="collapse" data-parent="#accordion<?php echo $key+1?>"><?php echo $s['negara']?></a></div>
+            <div class="accordion-body collapse" id="collapse<?php echo $key+1?>">
                 <div class="accordion-inner">
                     <table class="table">
                         <thead>
                             <tr>
                                 <td><b>No</b></td>
-                                <td class="span3"><b>Club</b></td>
-                                <td><b>Date 1</b><b></b></td>
-                                <td><b>Date 2</b></td>
-                                <td><b>Date 3</b></td>
+                                <td><b>Date 1(today)</b></td>
+                                <td><b>Date 2(tomorrow)</b></td>
+                                <td><b>Date 3(lusa)</b></td>
                                 <td><b>Date 4</b></td>
                                 <td><b>Date 5</b></td>
+                                 <td><b>Date 6</b></td>
+                                  <td><b>Date 7</b></td>
                             </tr>
                         </thead>
+                        <?php 
+                        
+						$perulangan=0;
+						foreach($s['row'] as $d)
+						{
+							$lg=sizeof($d);
+							if($perulangan<$lg)$perulangan=$lg;
+						}
+						
+                        ?>
+                        
                         <tbody>
+                        	<?php for ($i=0; $i < $perulangan; $i++) { ?>
                             <tr>
-                                <td>1</td>
-                                <td class="span3">Chelsea</td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td><?php echo $i+1?></td>
+
+                                <td><?php echo (!empty($s['row'][0][$i]['team'])) ? $s['row'][0][$i]['team'].'</br>'.$s['row'][0][$i]['time'] : '' ; ?></td>
+                                <td><?php echo (!empty($s['row'][1][$i]['team'])) ? $s['row'][1][$i]['team'].'</br>'.$s['row'][1][$i]['time'] : '' ; ?></td>
+                                <td><?php echo (!empty($s['row'][2][$i]['team'])) ? $s['row'][2][$i]['team'].'</br>'.$s['row'][2][$i]['time'] : '' ; ?></td>
+                                <td><?php echo (!empty($s['row'][3][$i]['team'])) ? $s['row'][3][$i]['team'].'</br>'.$s['row'][3][$i]['time'] : '' ; ?></td>
+                                <td><?php echo (!empty($s['row'][4][$i]['team'])) ? $s['row'][4][$i]['team'].'</br>'.$s['row'][4][$i]['time'] : '' ; ?></td>
+                                <td><?php echo (!empty($s['row'][5][$i]['team'])) ? $s['row'][5][$i]['team'].'</br>'.$s['row'][5][$i]['time'] : '' ; ?></td>
+                                <td><?php echo (!empty($s['row'][6][$i]['team'])) ? $s['row'][6][$i]['team'].'</br>'.$s['row'][6][$i]['time'] : '' ; ?></td>
+                                
                             </tr>
-                            <tr>
-                                <td>2</td>
-                                <td class="span3">Manchester United</td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td class="span3">Manchester City</td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
+                            <?php }?>
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
-        <div class="accordion" id="accordion3">
-            <div class="accordion-group">
-                <div class="accordion-heading"><a class="accordion-toggle" href="#collapse2" data-toggle="collapse" data-parent="#accordion3">Spanyol</a></div>
-                <div class="accordion-body collapse" id="collapse2">
-                    <div class="accordion-inner">
-                        <table class="table" id="veggiegrid">
-                            <thead>
-                                <tr>
-                                    <td><b>No</b></td>
-                                    <td class="span3"><b>Club</b></td>
-                                    <td><b>Date 1</b><b></b></td>
-                                    <td><b>Date 2</b></td>
-                                    <td><b>Date 3</b></td>
-                                    <td><b>Date 4</b></td>
-                                    <td><b>Date 5</b></td>
-                                </tr>
-                            </thead>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td class="span3">Barcelona</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
+	<?php }?>
+   
     </div>
 </div>
